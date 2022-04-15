01b073312c467a65d98ee22bdea45dfcf74e228a
Greg <gjhamlin@gmail.com>
2022-04-15 08:14:38 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "graphics.h"
+#include <iomanip>
+
+using namespace std;
+using namespace mssm;
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#endif
+
+// Note:  Mac Users who are using Qt Creator...
+//        if the program compiles and runs but a window doesn't open up
+//        try un-checking "run in terminal"
+
+
+// Here are some of the basic drawing commands:
+
+//void   line(Vec2d p1, Vec2d p2, Color c = WHITE);
+//void   ellipse(Vec2d center, double w, double h, Color c = WHITE, Color f = TRANSPARENT);
+//void   arc(Vec2d center, double w, double h, double a, double alen, Color c = WHITE);
+//void   chord(Vec2d center, double w, double h, double a, double alen, Color c = WHITE, Color f = TRANSPARENT);
+//void   pie(Vec2d center, double w, double h, double a, double alen, Color c = WHITE, Color f = TRANSPARENT);
+//void   rect(Vec2d corner, double w, double h, Color c = WHITE, Color f = TRANSPARENT);
+//void   polygon(std::vector<Vec2d> pts, Color border, Color fill = TRANSPARENT);
+//void   polyline(std::vector<Vec2d> pts, Color color);
+//void   text(Vec2d pos, double size, const std::string& str, Color textColor = WHITE, HAlign hAlign = HAlign::left, VAlign vAlign = VAlign::baseline);
+
+
+
+int main()
+{
+    Graphics g("MyProgram", 1024, 768);
+
+    while (g.draw()) {
+
+        g.polygon({{100, 200}, {20, 500}, {300, 10}}, GREEN, BLUE);
+
+        g.text({300,300}, 20, "Hello World");
+
+        g.points({{5,5},{10,10},{20,20},{50,50}, {100,100}}, YELLOW);
+
+        if (g.isKeyPressed(Key::ESC)) {
+            break;
+        }
+
+        for (const Event& e : g.events()) {
+            switch (e.evtType) {
+            case EvtType::KeyPress:
+                break;
+            case EvtType::KeyRelease:
+                break;
+            case EvtType::MouseMove:
+                break;
+            case EvtType::MousePress:
+                break;
+            case EvtType::MouseRelease:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
